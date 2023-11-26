dfedb3c602db1c8eba38a493577a78d748bcbb01
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-11-26 22:22:18 +0600

    first commit

--- generatAFn.js
+++ generatAFn.js
@@ -1,20 +1,41 @@
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
 
-
 // generator
-function* generator() {
-    yield 1;
-    yield 2;
-    yield 3;
-    yield 4;
-    yield 5;
+// function* generator() {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+//     yield 4;
+//     yield 5;
+// }
+
+// const myGen = generator();
+
+// console.log(myGen.next());
+// console.log(myGen.next());
+// console.log(myGen.next());
+// console.log(myGen.next());
+// console.log(myGen.next());
+// console.log(myGen.next());
+
+function* range(start = 0, stop = 100, step = 5) {
+  for (let i = start; i <= stop; i += step) {
+    yield i;
+  }
 }
+// const rangeof = range();
+// console.log(rangeof.next());
+// console.log(rangeof.next());
+// console.log(rangeof.next());
+// console.log(rangeof.next());
+// console.log(rangeof.next());
+// console.log(rangeof.next());
+// console.log(rangeof.next());
+// console.log(rangeof.next());
+// console.log(rangeof.next());
+// console.log(rangeof.next());
+// console.log(rangeof.next());
 
-const myGen = generator();
-
-console.log(myGen.next());
-console.log(myGen.next());
-console.log(myGen.next());
-console.log(myGen.next());
-console.log(myGen.next());
-console.log(myGen.next());
+for (let v of range()) {
+  console.log(v);
+}
